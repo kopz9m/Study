59f97b783527d00ed67b0e175f5376ee5b8da235
Patrick <zpc1996@hotmail.com>
2023-04-09 14:31:10 +0800

    1053

--- src/LeetCode/_1053_Previous_Permutation_With_One_Swap.java
+++ src/LeetCode/_1053_Previous_Permutation_With_One_Swap.java
@@ -1,7 +1,6 @@
 package LeetCode;
 
 
-
 /**
  * @Author: Patrick
  * @Date: 2023-04-03-17:38
@@ -12,13 +11,29 @@ public class _1053_Previous_Permutation_With_One_Swap {
 
         int len = arr.length;
 
-        for (int i = len - 1; i > 0; i--) {
+        int leftIndex = Integer.MIN_VALUE;
+        int rightIndex = Integer.MIN_VALUE;
+        int leftValue = Integer.MAX_VALUE;
+        int rightValue = Integer.MIN_VALUE;
 
-
-
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (arr[i] > arr[j]) {
+                    if (arr[j] > rightValue) {
+                        leftIndex = i;
+                        rightIndex = j;
+                        leftValue = arr[i];
+                        rightValue = arr[j];
+                    }
+                }
+            }
+            rightValue = Integer.MIN_VALUE;
         }
-
-        return new int[]{};
-
+        if (leftIndex >= 0) {
+            int tmp = arr[leftIndex];
+            arr[leftIndex] = arr[rightIndex];
+            arr[rightIndex] = tmp;
+        }
+        return arr;
     }
 }
